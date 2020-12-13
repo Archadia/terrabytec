a8f4e6c2e2a6e54eb1ec39f909a7636b67e1e3f2
Archadia <atroglyphics@protonmail.com>
2020-12-13 02:00:22 +0000

    init

---
+++ src/terrabyte.h
@@ -0,0 +1,129 @@
+#include <stdint.h>
+#include <string>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <chrono>
+#include "terrabyte_math.h"
+
+#define TERRABYTE_LOG "[TerrabyteEngine]"
+
+namespace pkr
+{    
+    struct TERRABYTE_ENGINE
+    {
+        GLFWwindow* window;
+    };
+
+    inline void EngineLog(const char* text)
+    {
+        printf("%s %s\n", TERRABYTE_LOG, text);
+    }
+
+    void SetEngineVSync(bool b)
+    {
+        glfwSwapInterval(b ? 1 : 0);
+    }
+
+    void SetWindowTitle(TERRABYTE_ENGINE& engine, std::string title)
+    {
+        glfwSetWindowTitle(engine.window, title.c_str());
+    }
+
+    VEC2D GetMousePosition(TERRABYTE_ENGINE& engine)
+    {
+        VEC2D vec;
+        glfwGetCursorPos(engine.window, &vec.x, &vec.y);
+        return vec;
+    }
+
+    VEC2F GetWindowContentScale(TERRABYTE_ENGINE& engine)
+    {
+        VEC2F vec;
+        glfwGetWindowContentScale(engine.window, &vec.x, &vec.y);
+        return vec;
+    }
+
+    VEC2I GetFrameBufferSize(TERRABYTE_ENGINE& engine)
+    {
+        VEC2I vec;
+        glfwGetFramebufferSize(engine.window, &vec.x, &vec.y);
+        return vec;
+    }
+
+    VEC2I GetWindowSize(TERRABYTE_ENGINE& engine)
+    {
+        VEC2I vec;
+        glfwGetWindowSize(engine.window, &vec.x, &vec.y);
+        return vec;
+    }
+
+    float GetWindowAspectRatio(TERRABYTE_ENGINE& engine)
+    {
+        VEC2I size = GetWindowSize(engine);
+        return (float) (size.x / size.y);
+    }
+
+    TERRABYTE_ENGINE* CreateEngine(uint32_t width, uint32_t height, std::string title)
+    {
+        uint32_t rInit = glfwInit();
+        if(rInit)
+        {
+            glfwDefaultWindowHints();
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+
+            TERRABYTE_ENGINE* engine = new TERRABYTE_ENGINE();
+            GLFWwindow* rWindow = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+            if(rWindow)
+            {
+                engine->window = rWindow;
+
+                glfwMakeContextCurrent(rWindow);
+                uint32_t rGlad = gladLoadGL();
+                if(rGlad)
+                {
+                    return engine;
+                } else EngineLog("Unable to load GLAD.");
+            }
+            else EngineLog("Unable to create GLFW window.");
+            delete engine;
+        }
+        else EngineLog("Unable to initialise GLFW.");
+        glfwTerminate();
+        return nullptr;
+    }
+
+    void StartEngine(TERRABYTE_ENGINE& engine, void (*update)(double dt), void (*draw)())
+    {
+        EngineLog("Starting TerrabyteEngine...");
+
+        const double dt = 0.01;
+
+        double startTime = std::chrono::high_resolution_clock::now().time_since_epoch().count() / 1000000000.0;
+        double accumulator = 0.0;
+
+        while(!glfwWindowShouldClose(engine.window))
+        {
+            double endTime = std::chrono::high_resolution_clock::now().time_since_epoch().count() / 1000000000.0;
+            double frameTime = (endTime - startTime);
+            startTime = endTime;
+
+            accumulator += frameTime;
+
+            while(accumulator >= dt)
+            {
+                update(dt);
+
+                accumulator -= dt;
+            }
+
+            glClear(GL_COLOR_BUFFER_BIT);
+            draw();
+            glfwSwapBuffers(engine.window);
+            glfwPollEvents();
+        }
+        EngineLog("Engine terminated.");
+        glfwTerminate();
+    }
+}
---
+++ src/terrabyte_math.h
@@ -0,0 +1,116 @@
+namespace pkr
+{
+    struct VEC2F
+    {
+        float x;
+        float y;
+
+        VEC2F operator+(const VEC2F& v)
+        {
+            VEC2F vec;
+            vec.x = x + v.x;
+            vec.y = y + v.y;
+            return vec;
+        }
+
+        VEC2F operator-(const VEC2F& v)
+        {
+            VEC2F vec;
+            vec.x = x - v.x;
+            vec.y = y - v.y;
+            return vec;
+        }
+
+        VEC2F operator*(const VEC2F& v)
+        {
+            VEC2F vec;
+            vec.x = x * v.x;
+            vec.y = y * v.y;
+            return vec;
+        }
+
+        VEC2F operator/(const VEC2F& v)
+        {
+            VEC2F vec;
+            vec.x = x / v.x;
+            vec.y = y / v.y;
+            return vec;
+        }
+    };
+
+    struct VEC2I
+    {
+        int32_t x;
+        int32_t y;
+
+        VEC2I operator+(const VEC2I& v)
+        {
+            VEC2I vec;
+            vec.x = x + v.x;
+            vec.y = y + v.y;
+            return vec;
+        }
+
+        VEC2I operator-(const VEC2I& v)
+        {
+            VEC2I vec;
+            vec.x = x - v.x;
+            vec.y = y - v.y;
+            return vec;
+        }
+
+        VEC2I operator*(const VEC2I& v)
+        {
+            VEC2I vec;
+            vec.x = x * v.x;
+            vec.y = y * v.y;
+            return vec;
+        }
+
+        VEC2I operator/(const VEC2I& v)
+        {
+            VEC2I vec;
+            vec.x = x / v.x;
+            vec.y = y / v.y;
+            return vec;
+        }
+    };
+
+    struct VEC2D
+    {
+        double x;
+        double y;
+
+        VEC2D operator+(const VEC2D& v)
+        {
+            VEC2D vec;
+            vec.x = x + v.x;
+            vec.y = y + v.y;
+            return vec;
+        }
+
+        VEC2D operator-(const VEC2D& v)
+        {
+            VEC2D vec;
+            vec.x = x - v.x;
+            vec.y = y - v.y;
+            return vec;
+        }
+
+        VEC2D operator*(const VEC2D& v)
+        {
+            VEC2D vec;
+            vec.x = x * v.x;
+            vec.y = y * v.y;
+            return vec;
+        }
+
+        VEC2D operator/(const VEC2D& v)
+        {
+            VEC2D vec;
+            vec.x = x / v.x;
+            vec.y = y / v.y;
+            return vec;
+        }
+    };
+}
